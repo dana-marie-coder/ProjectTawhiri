24314affe34ef6357817bdf8da436ed24a8fb0f0
dana-marie-coder <109443893+dana-marie-coder@users.noreply.github.com>
2024-05-05 13:56:56 +1000

    search function using api

--- source/script.js
+++ source/script.js
@@ -1,9 +1,25 @@
+function updateCityTemperature(response) {
+  let currentTemperature = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let cityTitle = document.querySelector("#weather-city");
+  cityTitle.innerHTML = response.data.city;
+  currentTemperature.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "5f66a5061f6to1243b6a5681aa6334d6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateCityTemperature);
+}
+
 function searchFormSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityTitle = document.querySelector("#weather-city");
-  cityTitle.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchFormSubmit);
+
+searchCity("Redcliffe");
